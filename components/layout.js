402ecd96a2bf2b35437f7238e81cebe7fcba76b3
vicheanak <vicheanak@gmail.com>
2021-12-20 15:59:30 +0700

    add category menu & hook data api

--- components/layout.js
+++ components/layout.js
@@ -38,14 +38,14 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header>
-        <nav class="bg-gray-200">
+        <nav class="bg-gradient-to-b from-yellow-200 to-gray-100">
           <div class="max-w-6xl mx-auto px-4">
             <div class="flex justify-between">
               <div class="flex space-x-4">
                 <div>
                   <a href="#" class="flex items-center py-5 px-3">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="text-blue-400 h-8 w-8 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                     <span class="font-bold">MMO Cambodia</span>
                   </a>
@@ -57,12 +57,12 @@ export default function Layout({ children, home }) {
                       Facebook
                     </a>
                   </Link>
-                  <Link href="facebook">
+                  <Link href="youtube">
                     <a class="py-5 px-3 text-red-700 hover:text-red-900 font-bold">
                       YouTube
                     </a>
                   </Link>
-                  <Link href="facebook">
+                  <Link href="crypto">
                     <a class="py-5 px-3 text-green-700 hover:text-green-900 font-bold">
                       Crypto
                     </a>
--- pages/facebook/index.js
+++ pages/facebook/index.js
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Layout, { siteTitle } from '../../components/layout'
 import Link from 'next/link'
-import getSortedPostsData from '../lib/posts'
-import Date from '../components/date'
+import getSortedPostsData from '../../lib/posts'
+import Date from '../../components/date'
 
 
 
@@ -23,9 +22,9 @@ export default function Home({allPostsData}) {
         <title>MMO Cambodia</title>
       </Head>
       <div class="flex max-w-6xl mx-auto mt-5">
-        <section class="md:block sm:hidden flex h-full w-50 flex-col justify-between rounded">
+        <section class="md:block sm:hidden flex min-w-50 h-full w-50 flex-col justify-between rounded">
           <Link href="facebook/accounts">
-            <a class="hover:bg-gradient-to-r from-cyan-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
+            <a class="hover:bg-gradient-to-r from-yellow-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
             </svg>
@@ -33,7 +32,7 @@ export default function Home({allPostsData}) {
             </a>
           </Link>
           <Link href="facebook/accounts">
-            <a class="hover:bg-gradient-to-r from-cyan-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
+            <a class="hover:bg-gradient-to-r from-yellow-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
             </svg>
@@ -41,15 +40,7 @@ export default function Home({allPostsData}) {
             </a>
           </Link>
           <Link href="facebook/accounts">
-            <a class="hover:bg-gradient-to-r from-cyan-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
-            </svg>
-            Likes & Watchtime
-            </a>
-          </Link>
-          <Link href="facebook/accounts">
-            <a class="hover:bg-gradient-to-r from-cyan-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
+            <a class="hover:bg-gradient-to-r from-yellow-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z" />
             </svg>
@@ -57,7 +48,7 @@ export default function Home({allPostsData}) {
             </a>
           </Link>
           <Link href="facebook/accounts">
-            <a class="hover:bg-gradient-to-r from-cyan-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
+            <a class="hover:bg-gradient-to-r from-yellow-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
             </svg>
@@ -65,31 +56,33 @@ export default function Home({allPostsData}) {
             </a>
           </Link>
           <Link href="facebook/accounts">
-            <a class="hover:bg-gradient-to-r from-cyan-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
+            <a class="hover:bg-gradient-to-r from-yellow-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
-            Tools, VPN, VPS
+            Tools, VPS
             </a>
           </Link>
           <Link href="facebook/accounts">
-            <a class="hover:bg-gradient-to-r from-cyan-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
+            <a class="hover:bg-gradient-to-r from-yellow-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
-            </svg>
-            Devices
-            </a>
-          </Link>
-          <Link href="facebook/accounts">
-            <a class="hover:bg-gradient-to-r from-cyan-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
             </svg>
             Lessons
             </a>
           </Link>
           <Link href="facebook/accounts">
-            <a class="hover:bg-gradient-to-r from-cyan-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
+            <a class="hover:bg-gradient-to-r from-yellow-100 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+            </svg>
+            Others
+            </a>
+          </Link>
+          <Link href="facebook/accounts">
+            <a class="hover:bg-gradient-to-r from-yellow-100 text-red-700 to-gray-100 px-10 py-2 m-2 flex rounded transition duration-300 items-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
             </svg>
@@ -98,27 +91,23 @@ export default function Home({allPostsData}) {
           </Link>
         </section>
         <section class="max-w-3xl">
-          <div class="border-b p-4">Homepage > Facebook</div>
-          {allPostsData.map(({ id, attributes }) => (
-            <Link href={`posts/${id}`}>
-              <a class="hover:bg-yellow-100 max-h-30 p-4 h-30 flex-row flex rounded overflow-hidden shadow-lg my-2">
-                <img
-                  class="w-40"
-                  src="https://tailwindcss.com/img/card-top.jpg"
-                  alt="Sunset in the mountains"
-                />
-                <div class="px-6 py-4 w-screen">
-                  <div class="font-bold text-xl mb-2">{attributes.title}</div>
-                  <p className="text-grey-darker text-base">
-                    {attributes.description}
-                  </p>
-                  <p className="text-xs mt-2">
-                    <Date dateString={attributes.createdAt}></Date>
-                  </p>
-                </div>
-              </a>
-            </Link>
-          ))}
+          <div class="border-b p-3 text-blue-600 font-bold">Facebook</div>
+          <Link href="post">
+            <a class="hover:bg-gray-200 max-h-30 p-4 h-30 flex-row flex rounded overflow-hidden shadow-lg my-2">
+              <img
+                class="w-40"
+                src="https://tailwindcss.com/img/card-top.jpg"
+                alt="Sunset in the mountains"
+              />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">Next + Tailwind ❤️</div>
+                <p className="text-grey-darker text-base">
+                  Next and Tailwind CSS are a match made in heaven, check out this article on how
+                  you can combine these two for your next app.
+                </p>
+              </div>  
+            </a>
+          </Link>
           
           {/* <h2 className={utilStyles.headingLg}>Blog</h2>
           <ul className={utilStyles.list}>
